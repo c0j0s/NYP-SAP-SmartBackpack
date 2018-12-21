cf39ae143bd93cdb02eb511951dd41259e05686f
c0j0s <c.junsheng@hotmail.com>
2018-12-21 17:21:12 +0800

    Implment CRUD in admin portal

--- SmartBackpackSAP/HANA_Webapp/SmartBackpackAdminPortal/webapp/controller/Detail.controller.js
+++ SmartBackpackSAP/HANA_Webapp/SmartBackpackAdminPortal/webapp/controller/Detail.controller.js
@@ -3,8 +3,12 @@ sap.ui.define([
 	"sbp/SmartBackpackAdmin/controller/BaseController",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device",
+	"sap/m/Dialog",
+	"sap/m/Button",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast",
 	"sbp/SmartBackpackAdmin/model/formatter"
-], function (BaseController, JSONModel, formatter) {
+], function (BaseController, JSONModel, formatter, Dialog, Button, MessageBox, MessageToast) {
 	"use strict";
 	return BaseController.extend("sbp.SmartBackpackAdmin.controller.Detail", {
 		formatter: formatter,
@@ -20,18 +24,8 @@ sap.ui.define([
 				delay: 0
 			});
 			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
-			this.setModel(oViewModel, "detailView"); 
+			this.setModel(oViewModel, "detailView");
 			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
-			//var bindingContext = this.getOwnerComponent().getBindingContext();
-			//var userid = bindingContext.getProperty("USER_ID");
-			//console.log(userid);
-			// var userDeviceTable = this.getView().byId("userDeviceTable");
-			// var oFilter = new sap.ui.model.Filter("USER_ID", sap.ui.model.FilterOperator.EQ, '{USER_ID}');
-			// userDeviceTable.bindItems({
-			// 	path: "/userDevices",
-			// 	template: userDeviceTable.getBindingInfo("items").template,
-			// 	filters: [oFilter]
-			// });
 		},
 		/* =========================================================== */
 		/* event handlers                                              */
@@ -74,6 +68,7 @@ sap.ui.define([
 		_onObjectMatched: function (oEvent) {
 			var sObjectId = oEvent.getParameter("arguments").objectId;
 			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+			this.getModel("appView").setProperty("/detailsexpended", true);
 			this.getModel().metadataLoaded().then(function () {
 				var sObjectPath = this.getModel().createKey("user", {
 					USER_ID: sObjectId
@@ -157,6 +152,7 @@ sap.ui.define([
 		 */
 		onCloseDetailPress: function () {
 			this.getModel("appView").setProperty("/actionButtonsInfo/midColumn/fullScreen", false);
+			this.getModel("appView").setProperty("/detailsexpended", false);
 			// No item should be selected on master after detail page is closed
 			this.getOwnerComponent().oListSelector.clearMasterListSelection();
 			this.getRouter().navTo("master");
@@ -181,23 +177,342 @@ sap.ui.define([
 		 */
 		expandIotDataOverview: function (oEvent) {
 			var sObjectId = this.getModel("detailView").getProperty("/userid");
-			console.log(sObjectId);
+			//console.log(sObjectId);
 			var DEVICE_SN = oEvent.getSource().data("device-sn");
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("userDeviceOvp", {
-				userid : sObjectId,
-				devicesn : DEVICE_SN
+				userid: sObjectId,
+				devicesn: DEVICE_SN
 			});
-			// console.log(DEVICE_SN);
-			// this.getView().byId("iotDataOverviewPanel").setExpanded(true);
-			// this.getView().byId("iotDataLabelInfo").setVisible(false);
-			// var iotDataTable = this.getView().byId("iotDataTable");
-			// iotDataTable.setVisible(true);
-			// iotDataTable.bindItems({
-			// 	path: "/iotData",
-			// 	template: iotDataTable.getBindingInfo("items").template,
-			// 	filters: [new sap.ui.model.Filter("DEVICE_SN", sap.ui.model.FilterOperator.EQ, DEVICE_SN)]
-			// });
+		},
+		pressDialog: null,
+		/**
+		 *@memberOf sbp.SmartBackpackAdmin.controller.Detail
+		 */
+		openEditProfileDialog: function (oEvent) {
+			var sPath = this.getView().getElementBinding().getPath();
+			var oObject = this.getView().getModel().getObject(sPath);
+			var oModel = this.getView().getModel();
+
+			this.pressDialog = new Dialog({
+				title: "Update user profile",
+				type: "Message",
+				contentWidth: "410px",
+				content: [
+					new sap.m.Label({
+						text: "Personal Details",
+						design: "Bold",
+						textAlign: "Center"
+					}),
+					new sap.m.VBox({}),
+					new sap.m.Label({
+						required : true,
+						text: "Name",
+						labelFor: "input-username"
+					}),
+					new sap.m.Input("input-username", {
+						type: "Text",
+						value: oObject.NAME
+					}),
+					new sap.m.Label({
+						required : true,
+						text: "Gender",
+						labelFor: "input-gender"
+					}),
+					new sap.m.RadioButtonGroup("input-gender", {
+						required : true,
+						selectedIndex: ((oObject.GENDER === "M") ? 0 : 1),
+						columns: 1,
+						buttons: [
+							new sap.m.RadioButton({
+								text: "Male"
+							}),
+							new sap.m.RadioButton({
+								text: "Female"
+							})
+						]
+					}),
+					new sap.m.VBox({}),
+					new sap.m.Label({
+						text: "Race",
+						labelFor: "input-race"
+					}),
+					new sap.m.Input("input-race", {
+						type: "Text",
+						value: oObject.RACE
+					}),
+					new sap.m.Label({
+						required : true,
+						text: "Date of Birth",
+						labelFor: "input-dob"
+					}),
+					new sap.m.DatePicker("input-dob", {
+						displayFormat: "long",
+						value: new Date(oObject.DOB).toDateString()
+					}),
+					new sap.m.Label({
+						required : true,
+						text: "Asthmatic level",
+						labelFor: "input-asthlevel"
+					}),
+					new sap.m.Slider("input-asthlevel", {
+						enableTickmarks: true,
+						min: 0,
+						max: 4,
+						value: oObject.ASTHMATIC_LEVEL,
+						liveChange: this.handleAsthmaticSliderChanges,
+						width: "100%"
+					}),
+					new sap.m.Text("input-asthlevel-info", {
+						text: oObject.ASTHMATIC_LEVEL
+					}),
+					new sap.m.VBox({}),
+					new sap.m.Label({
+						text: "Contact information",
+						design: "Bold",
+						textAlign: "Center"
+					}),
+					new sap.m.VBox({}),
+					new sap.m.Label({
+						text: "Email",
+						labelFor: "input-email"
+					}),
+					new sap.m.Input("input-email", {
+						type: "Email",
+						value: oObject.EMAIL
+					}),
+					new sap.m.Label({
+						text: "Contact number",
+						labelFor: "input-contactno"
+					}),
+					new sap.m.Input("input-contactno", {
+						type: "Number",
+						value: oObject.CONTACT_NO
+					})
+				],
+				beginButton: new Button({
+					text: "Update Profile",
+					press: function () {
+						var asthlevel = sap.ui.getCore().byId("input-asthlevel").getValue();
+						var contactnum = sap.ui.getCore().byId("input-contactno").getValue();
+						var email = sap.ui.getCore().byId("input-email").getValue();
+						var username = sap.ui.getCore().byId("input-username").getValue();
+						var race = sap.ui.getCore().byId("input-race").getValue();
+						var gender = sap.ui.getCore().byId("input-gender").getSelectedIndex();
+						var dob = new Date(sap.ui.getCore().byId("input-dob").getValue()).getTime();
+						var oEntry = {
+							ASTHMATIC_LEVEL: asthlevel,
+							CONTACT_NO: contactnum.toString(),
+							EMAIL: email,
+							NAME: username,
+							RACE: race,
+							GENDER: (gender === 0) ? "M" : "F",
+							DOB: "/Date(" + dob + ")/"
+						};
+						console.log(oEntry);
+
+						oModel.update("/user('" + oObject.USER_ID + "')", oEntry, {
+							success: function () {
+								MessageToast.show("Profile Updated");
+							},
+							error: function () {
+								MessageToast.show("Fail to Add New Device");
+							}
+						});
+						this.pressDialog.destroy();
+					}.bind(this)
+				}),
+				endButton: new Button({
+					text: "Close",
+					press: function () {
+						this.pressDialog.destroy();
+					}.bind(this)
+				})
+			});
+			//to get access to the global model
+			this.getView().addDependent(this.pressDialog);
+
+			this.pressDialog.open();
+		},
+		handleAsthmaticSliderChanges: function (oEvent) {
+			var leveldesc = [
+				"level 0 - Nil",
+				"level 1 - Intermittent Asthma",
+				"level 2 - Mild Persistent Asthma",
+				"level 3 - Moderate Persistent Asthma",
+				"level 4 - Severe Persistent Asthma"
+			];
+			var level = oEvent.getParameters().value;
+			sap.ui.getCore().byId("input-asthlevel-info").setText(leveldesc[level]);
+		},
+		/**
+		 *@memberOf sbp.SmartBackpackAdmin.controller.Detail
+		 */
+		openAddDeviceDialog: function (oEvent) {
+			var userid = this.getModel("detailView").getProperty("/userid");
+			var userDeviceTable = this.getView().byId("userDeviceTable");
+			var oModel = userDeviceTable.getModel();
+			var timeint = new Date().getTime();
+
+			this.pressDialog = new Dialog({
+				title: "Add device to user",
+				type: "Message",
+				contentWidth: "400px",
+				content: [
+					new sap.m.Label({
+						text: "Device Serial Number",
+						labelFor: "input-deviceSn"
+					}),
+					new sap.m.Input("input-deviceSn", {
+						required : true,
+						type: "Text",
+						placeholder: "Enter Device Serial Number",
+						showSuggestion: true,
+						showValueHelp: true,
+						valueHelpRequest: [this.handleValueHelp, this],
+						valueHelpOnly: true,
+					}),
+					new sap.m.Label({
+						text: "Device Name",
+						labelFor: "input-deviceName"
+					}),
+					new sap.m.Input("input-deviceName", {
+						type: "Text"
+					}),
+					new sap.m.Label({
+						text: "Device Configurations",
+						design: "Bold",
+						textAlign: "Center"
+					}),
+					new sap.m.VBox({}),
+					new sap.m.Label({
+						text: "Minutes to record sensor readings",
+						labelFor: "input-minutestorecord"
+					}),
+					new sap.m.Input("input-minutestorecord", {
+						type: "Number",
+						value: 10
+					}),
+					new sap.m.CheckBox("input-enablebuzzer", {
+						text: "Enable Buzzer ",
+						selected: true
+					}),
+					new sap.m.CheckBox("input-enableled", {
+						text: "Enable LED [NOT IMPLEMENTED]",
+						selected: true
+					}),
+				],
+				beginButton: new Button({
+					text: "Add Device",
+					press: function () {
+						var devicesn = sap.ui.getCore().byId("input-deviceSn").getValue();
+						var devicename = sap.ui.getCore().byId("input-deviceName").getValue();
+						var minutestorecord = sap.ui.getCore().byId("input-minutestorecord").getValue();
+						var enablebuzzer = sap.ui.getCore().byId("input-enablebuzzer").getSelected();
+						var enableled = sap.ui.getCore().byId("input-enableled").getSelected();
+						if(devicename === ""){
+							devicename = "new bag";
+						}
+						if(minutestorecord === ""){
+							minutestorecord = 10;
+						}
+						var oEntry = {
+							USER_ID: userid,
+							DEVICE_SN: devicesn,
+							DEVICE_NAME: devicename,
+							REGISTERED_ON: "/Date(" + timeint + ")/",
+							MINUTES_TO_RECORD_DATA: Number(minutestorecord),
+							CONFIG_ENABLE_BUZZER: (enablebuzzer) ? "Y" : "N",
+							CONFIG_ENABLE_LED: (enableled) ? "Y" : "N",
+							CONFIG_UPDATED_ON: "/Date(" + timeint + ")/",
+						};
+						console.log(oEntry);
+
+						oModel.create("/userDevices", oEntry, {
+							success: function () {
+								MessageToast.show("New Device Added");
+							},
+							error: function () {
+								MessageToast.show("Fail to Add New Device");
+							}
+						});
+						this.pressDialog.destroy();
+					}.bind(this)
+				}),
+				endButton: new Button({
+					text: "Close",
+					press: function () {
+						this.pressDialog.destroy();
+					}.bind(this)
+				})
+			});
+			//to get access to the global model
+			this.getView().addDependent(this.pressDialog);
+
+			this.pressDialog.open();
+		},
+		handleValueHelp: function (oController) {
+
+			this.inputId = oController.getSource().getId();
+			// create value help dialog
+			if (!this._valueHelpDialog) {
+				this._valueHelpDialog = sap.ui.xmlfragment(
+					"sbp.SmartBackpackAdmin.fragment.inputDeviceSNValueHelp",
+					this
+				);
+				this.getView().addDependent(this._valueHelpDialog);
+			}
+
+			this._valueHelpDialog.open();
+		},
+		_handleValueHelpSearch: function (oEvent) {
+			var sTerm = oEvent.getParameter("value");
+			var aFilters = [];
+			if (sTerm) {
+				aFilters.push(new sap.ui.model.Filter("DEVICE_SN", sap.ui.model.FilterOperator.Contains, sTerm));
+			}
+			oEvent.getSource().getBinding("items").filter(aFilters);
+		},
+		_handleValueHelpClose: function (evt) {
+			var oSelectedItem = evt.getParameter("selectedItem");
+
+			if (oSelectedItem) {
+				var productInput = sap.ui.getCore().byId("input-deviceSn");
+
+				productInput.setValue(oSelectedItem.getTitle());
+			}
+			evt.getSource().getBinding("items").filter([]);
+		},
+		/**
+		 *@memberOf sbp.SmartBackpackAdmin.controller.Detail
+		 */
+		openDeleteConfirmDialog: function (oEvent) {
+			var devicesn = oEvent.getSource().data("device-sn");
+			var userid = this.getModel("detailView").getProperty("/userid");
+			var userDeviceTable = this.getView().byId("userDeviceTable");
+			var oModel = userDeviceTable.getModel();
+
+			var bCompact = !!this.getView().$().closest(".sapUiSizeCompact").length;
+			MessageBox.confirm(
+				"Remove Device?", {
+					styleClass: bCompact ? "sapUiSizeCompact" : "",
+					onClose: function (sAction) {
+						if (sAction === "OK") {
+							oModel.remove("/userDevices(USER_ID='" + userid + "',DEVICE_SN='" + devicesn + "')", {
+								success: function () {
+									MessageToast.show("Delete successful");
+								},
+								error: function () {
+									MessageToast.show("Delete failed");
+								}
+
+							});
+
+						}
+					}
+				}
+
+			);
 		}
 	});
 });
--- SmartBackpackSAP/HANA_Webapp/SmartBackpackAdminPortal/webapp/controller/DeviceOverview.controller.js
+++ SmartBackpackSAP/HANA_Webapp/SmartBackpackAdminPortal/webapp/controller/DeviceOverview.controller.js
@@ -2,10 +2,13 @@ sap.ui.define([
 	"sbp/SmartBackpackAdmin/controller/BaseController",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/Device",
+	"sap/m/Dialog",
+	"sap/m/Button",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast",
 	"sbp/SmartBackpackAdmin/model/formatter"
-], function (BaseController, JSONModel, formatter) {
+], function (BaseController, JSONModel, formatter, Dialog, Button, MessageBox, MessageToast) {
 	"use strict";
-
 	return BaseController.extend("sbp.SmartBackpackAdmin.controller.DeviceOverview", {
 		formatter: formatter,
 		/**
@@ -13,16 +16,15 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf sbp.SmartBackpackAdmin.view.DeviceOverview
 		 */
-		onInit: function() {
+		onInit: function () {
 			var oViewModel = new JSONModel({
 				busy: false,
 				delay: 0
 			});
 			this.getRouter().getRoute("userDeviceOvp").attachPatternMatched(this._onObjectMatched, this);
-			this.setModel(oViewModel, "deviceOvp"); 
+			this.setModel(oViewModel, "deviceOvp");
 			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
 		},
-
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
@@ -31,7 +33,6 @@ sap.ui.define([
 		//	onBeforeRendering: function() {
 		//
 		//	},
-
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
@@ -40,7 +41,6 @@ sap.ui.define([
 		//	onAfterRendering: function() {
 		//
 		//	},
-
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
 		 * @memberOf sbp.SmartBackpackAdmin.view.DeviceOverview
@@ -48,13 +48,10 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
-		
 		_onObjectMatched: function (oEvent) {
 			var objectId = oEvent.getParameter("arguments").userid;
 			var devicesn = oEvent.getParameter("arguments").devicesn;
-			
 			//UPDATE to use combinded view
-			
 			this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
 			this.getModel().metadataLoaded().then(function () {
 				var sObjectPath = this.getModel().createKey("userDevices", {
@@ -63,13 +60,11 @@ sap.ui.define([
 				});
 				this._bindView("/" + sObjectPath);
 			}.bind(this));
-			
 			var aboutDevicePanel = this.getView().byId("aboutDevicePanelContent");
 			var devicePath = this.getModel().createKey("/iotDevice", {
-					DEVICE_SN: devicesn
-				});
+				DEVICE_SN: devicesn
+			});
 			aboutDevicePanel.bindElement(devicePath);
-			
 			var iotDataTable = this.getView().byId("iotDataTable");
 			iotDataTable.bindItems({
 				path: "/iotData",
@@ -77,7 +72,7 @@ sap.ui.define([
 				filters: [
 					new sap.ui.model.Filter("DEVICE_SN", sap.ui.model.FilterOperator.EQ, devicesn),
 					new sap.ui.model.Filter("USER_ID", sap.ui.model.FilterOperator.EQ, objectId)
-					]
+				]
 			});
 		},
 		_bindView: function (sObjectPath) {
@@ -161,7 +156,86 @@ sap.ui.define([
 				this.getModel("appView").setProperty("/layout", this.getModel("appView").getProperty("/previousLayout"));
 			}
 		},
+		/**
+		 *@memberOf sbp.SmartBackpackAdmin.controller.DeviceOverview
+		 */
+		openCustomiseDeviceDialog: function (oEvent) {
+			var sPath = this.getView().getElementBinding().getPath();
+			var oObject = this.getView().getModel().getObject(sPath);
+			var oModel = this.getView().getModel();
 
+			var pressDialog = new Dialog({
+					title: "Update Device Settigs",
+					type: "Message",
+					contentWidth:"400px",
+					content: [
+						new sap.m.Label({
+							text:"Device Name",
+							labelFor: "input-deviceName"
+						}),
+						new sap.m.Input("input-deviceName",{
+							type:"Text",
+							value: oObject.DEVICE_NAME
+						}),
+						new sap.m.Label({
+							text:"Device Configurations",
+							design:"Bold",
+							textAlign:"Center"
+						}),
+						new sap.m.VBox({
+						}),
+						new sap.m.Label({
+							text:"Minutes to record sensor readings",
+							labelFor: "input-minutestorecord"
+						}),
+						new sap.m.Input("input-minutestorecord",{
+							type:"Number",
+							value:oObject.MINUTES_TO_RECORD_DATA
+						}),
+						new sap.m.CheckBox("input-enablebuzzer",{
+							text:"Enable Buzzer ",
+							selected:((oObject.CONFIG_ENABLE_BUZZER === "Y") ? true : false)
+						}),
+						new sap.m.CheckBox("input-enableled",{
+							text:"Enable LED [NOT IMPLEMENTED]",
+							selected:((oObject.CONFIG_ENABLE_LED === "Y") ? true : false)
+						}),
+						],
+					beginButton: new Button({
+						text: "Update Configurations",
+						press: function () {
+							var devicename = sap.ui.getCore().byId("input-deviceName").getValue();
+							var minutestorecord = sap.ui.getCore().byId("input-minutestorecord").getValue();
+							var enablebuzzer = sap.ui.getCore().byId("input-enablebuzzer").getSelected();
+							var enableled = sap.ui.getCore().byId("input-enableled").getSelected();
+							var oEntry = {
+								DEVICE_NAME:devicename,
+								MINUTES_TO_RECORD_DATA:Number(minutestorecord),
+								CONFIG_ENABLE_BUZZER:(enablebuzzer) ? "Y" : "N",
+								CONFIG_ENABLE_LED:(enableled) ? "Y" : "N",
+								CONFIG_UPDATED_ON:"/Date(" + new Date().getTime() + ")/"
+							};
+							console.log(oEntry);
+							
+							oModel.update("/userDevices(USER_ID='" + oObject.USER_ID + "',DEVICE_SN='" + oObject.DEVICE_SN + "')", oEntry, { 
+						    success: function() { 
+						        MessageToast.show("Device Confguration Updated");
+						    },
+						    error: function() { 
+						        MessageToast.show("Fail to Update Device Configuration");
+						    }
+							});
+							pressDialog.destroy();
+						}.bind(this)
+					}),
+					endButton: new Button({
+						text: "Close",
+						press: function () {
+							pressDialog.destroy();
+						}
+					})
+			});
+			pressDialog.open();
+		}
 	});
-
 });
