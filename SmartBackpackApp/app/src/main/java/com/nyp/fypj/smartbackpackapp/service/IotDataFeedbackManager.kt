232725e9c7b3e1209dfd24e6eaf5002c593affb9
c0j0s <c.junsheng@hotmail.com>
2019-01-18 12:23:51 +0800

    Add set data feedback wrapper functions in Android

---
+++ SmartBackpackApp/app/src/main/java/com/nyp/fypj/smartbackpackapp/service/IotDataFeedbackManager.kt
@@ -0,0 +1,48 @@
+package com.nyp.fypj.smartbackpackapp.service
+
+import com.sap.cloud.android.odata.sbp.IotDataType
+import java.lang.RuntimeException
+import java.security.InvalidParameterException
+
+class IotDataFeedbackManager(private var sapServiceManager: SAPServiceManager,USER_ID:String,DATA_ID:Long) {
+
+    private var data:IotDataType? = null
+
+    private var feedbackDescription:HashMap<Int,String> = hashMapOf(
+            Pair(0,"Very Good"),
+            Pair(1,"Ok"),
+            Pair(2,"Uncomfortable"),
+            Pair(3,"Very Uncomfortable"),
+            Pair(4,"Danger")
+    )
+
+    init {
+        data = IotDataType()
+        data!!.rememberOld()
+        data!!.userId = USER_ID
+        data!!.dataId = DATA_ID
+    }
+
+    //NOT TESTED
+    fun setDataFeedback(level:Int,success:() -> Unit,error:(e:RuntimeException) -> Unit) {
+        if (level in 0..4) {
+            sapServiceManager!!.openODataStore {
+                sapServiceManager.getsbp().updateEntityAsync(data, {
+                    success()
+                }, { e: RuntimeException ->
+                    error(e)
+                })
+            }
+        } else {
+            throw InvalidParameterException()
+        }
+    }
+
+    fun getFeedbackLabel(level:Int):String{
+        if (level in 0..4) {
+            return feedbackDescription[level]!!
+        } else {
+            throw InvalidParameterException()
+        }
+    }
+}
