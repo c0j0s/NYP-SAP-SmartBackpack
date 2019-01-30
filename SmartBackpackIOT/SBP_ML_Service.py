67706f113149c76a0e67e00476801cefca6e487b
c0j0s <c.junsheng@hotmail.com>
2019-01-30 11:34:23 +0800

    Recover ML service script

--- SmartBackpackIOT/SBP_ML_Service.py
+++ SmartBackpackIOT/SBP_ML_Service.py
@@ -1,13 +1,11 @@
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
-from numpy import array
+import numpy as np
 
 def main():
-    data = [0.48,0.67,2.52,1.84,0.2]
-    shapedata = array(data)
-    print(shapedata.shape)
-    #data = tf.convert_to_tensor(data, dtype=tf.float32, name="input")
+    data = [[0.48,0.67,2.52,1.84,0.2]]
+    shapedata = np.array(data)
     model = keras.models.load_model('./model/sbp_model.h5')
     predictions = model.predict(shapedata)
     print(np.argmax(predictions[0]))
